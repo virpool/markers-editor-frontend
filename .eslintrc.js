d12e4fd0f7e480b27e8d5bad01d54849b92d69b7
Victor Konovalov <zzz42leo@gmail.com>
2021-10-18 16:04:28 +0300

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "globals": {
+        "google": "readonly",
+        "process": "readonly",
+        "Splide": "readonly",
+        "Selectr": "readonly"
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+};
